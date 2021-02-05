bf158edb5b5e4cf1dc4c5b4869ebab58b1cab51e
tani <berukoko1220-blog@yahoo.co.jp>
2021-02-05 15:03:32 +0900

    :seedling: Add dataset file.

---
+++ src/dataset.js
@@ -0,0 +1,90 @@
+const dataset = {
+  firstAnswer: {
+    answers: [
+      { content: "お話したい", nextId: "talk" },
+      { content: "私について知りたい", nextId: "aboutMe" },
+      { content: "お友達になりたい", nextId: "friend" },
+    ],
+    question: "Hello!!たにbotです。ご用件はなんでしょうか？",
+  },
+  talk: {
+    answers: [
+      { content: "お仕事の依頼について", nextId: "job" },
+      { content: "相談に乗って欲しい", nextId: "consultation" },
+    ],
+    question: "どのようなお話しましょうか？🙂",
+  },
+  job: {
+    answers: [
+      { content: "Webサイトを制作してほしい", nextId: "website" },
+      { content: "Webサイトを開発してほしい", nextId: "webapp" },
+      { content: "その他", nextId: "other" },
+    ],
+    question: "どのようなお仕事でしょうか？",
+  },
+  website: {
+    answers: [
+      { content: "問い合わせる", nextId: "contact" },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question: "Webサイト細作についてですね。コチラからお問い合わせできます。",
+  },
+  webapp: {
+    answers: [
+      { content: "問い合わせる", nextId: "contact" },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question: "Webアプリ開発についてですね。コチラからお問い合わせできます。",
+  },
+  other: {
+    answers: [
+      { content: "問い合わせる", nextId: "contact" },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question: "その他についてですね。コチラからお問い合わせできます。",
+  },
+  consultation: {
+    answers: [
+      { content: "DMする", nextId: "https://twitter.com/tani_ningen" },
+      { content: "匿名で相談したい", nextId: "anonymous" },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question:
+      "相談ですね😄人生相談でも愚痴でも何でもOKです！良かったらDMして下さい！",
+  },
+  anonymous: {
+    answers: [
+      {
+        content: "Slackの招待を貰う",
+        nextId:
+          "https://join.slack.com/t/w1612258522-9kv186738/shared_invite/zt-ltul6cg6-xxDw869g_AMdXNCDAAJ3LA",
+      },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question:
+      "匿名相談ですね！私のSlackチャンネルにて匿名で登録後、気軽に話しかけて下さい🥰",
+  },
+  aboutMe: {
+    answers: [
+      { content: "ポートフォリオを見る", nextId: "https://tani.im" },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question:
+      "私はフロントエンジニアを目指して学習している者です♪現在はReact,Nextを勉強しています。",
+  },
+  friend: {
+    answers: [
+      { content: "フォローする", nextId: "https://twitter.com/tani_ningen" },
+      {
+        content: "slackでお話する",
+        nextId:
+          "https://join.slack.com/t/w1612258522-9kv186738/shared_invite/zt-ltul6cg6-xxDw869g_AMdXNCDAAJ3LA",
+      },
+      { content: "最初の質問に戻る", nextId: "firstAnswer" },
+    ],
+    question:
+      "ぜひお友達になりましょう！気軽にお話しませんか？DMまたはslackから声かけて下さい😊",
+  },
+};
+
+export default dataset;
